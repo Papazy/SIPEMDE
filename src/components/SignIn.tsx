16e7035e33b4dbe4c45ea1412b0a0960b9b20504
dhaniansyahr <dhaniansyahr@gmail.com>
2023-12-14 14:13:06 +0700

    fix: Sign In custom return user

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -19,30 +19,6 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
   const [nik, setnik] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
 
-  // const handleLogin = async () => {
-  //   setIsLoading(true);
-
-  //   const response = await signIn("credentials", {
-  //     nik,
-  //     password,
-  //     redirect: false,
-  //   });
-
-  //   if (response?.error) {
-  //     toast({
-  //       title: "Login Failed",
-  //       description: "You have entered the wrong nik or password",
-  //     });
-  //   } else {
-  //     toast({
-  //       title: "Login Success",
-  //       description: "You have successfully logged in",
-  //     });
-  //   }
-
-  //   setIsLoading(false);
-  // };
-
   return (
     <div
       className={cn("flex flex-col gap-2 justify-center", className)}
@@ -51,14 +27,14 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
       <div className="flex flex-col gap-2">
         <div>
           <label
-            htmlFor="NIK-input"
+            htmlFor="NIK"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             NIK
           </label>
           <input
             type="text"
-            id="NIK-input"
+            id="NIK"
             value={nik}
             onChange={(e) => setnik(e.target.value)}
             className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -84,7 +60,10 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
           type="button"
           size="sm"
           className="w-full"
-          onClick={() => signIn("credentials", { nik, password })}
+          onClick={() => {
+            signIn("credentials", { nik: nik, password: password });
+            console.log("nik", nik, "password", password);
+          }}
           disabled={isLoading}
         >
           {isLoading ? null : "Login"}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,38 +1,35 @@
 import { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { prisma } from "../lib/prisma";
-// import Providers from "next-auth/providers/credentials";
 import Credentials from "next-auth/providers/credentials";
 import { PrismaClient } from "@prisma/client";
 import { db } from "@/lib/db";
-// import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 import { getServerSession } from "next-auth";
-// import { Provider } from "@radix-ui/react-toast";
-// import Providers from "@/components/Providers";
 
 const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     Credentials({
       name: "credentials",
 
       credentials: {
         nik: { label: "NIK", type: "text" },
-        password: { label: "Password", type: "password" },
       },
 
       async authorize(credentials, req) {
-        const user = await prisma.user.findFirst({
+        const user = await prisma.user.findUnique({
           where: {
             nik: credentials?.nik,
-            // password: credentials?.password,
           },
         });
-        if (user) {
-          return Promise.resolve(user);
+        if (user && credentials?.nik === user?.nik) {
+          // console.log(user);
+          return user;
         } else {
           return Promise.resolve(null);
         }
@@ -41,7 +38,7 @@ export const authOptions: NextAuthOptions = {
   ],
 
   pages: {
-    signIn: "/auth/login",
+    signIn: "/sign-in",
   },
 
   callbacks: {
@@ -69,23 +66,9 @@ export const authOptions: NextAuthOptions = {
         return token;
       }
 
-      if (!dbUser.username) {
-        await db.user.update({
-          where: {
-            id: dbUser.id,
-          },
-          data: {
-            username: nanoid(10),
-          },
-        });
-      }
-
       return {
         id: dbUser.id,
         name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-        username: dbUser.username,
       };
     },
     redirect() {
